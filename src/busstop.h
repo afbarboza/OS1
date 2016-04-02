2a3f8619b94cf3c83d19c8e8087218c2c73ca894
afbarboza <alex.barboza@usp.br>
2016-04-02 18:51:02 -0300

    redefined header file

--- src/busstop.h
+++ src/busstop.h
@@ -1,9 +1,8 @@
 #ifndef	STOPBUS_H
 #define	STOPBUS_H
 
-#define ENOPASSENGER	0
-#define	ENODOOR		0
-#define	EBUSYBYS	1
+#define ENOPASSENGER	0	/*no passenger is at bus*/
+#define	ENODOOR		0	/*the busstop door is closed*/
 
 #include <stdint.h>
 #include <pthread.h>
@@ -17,8 +16,7 @@ typedef struct busstop	busstop_t;
 *	struct busstop - stores all the busstop data.
 *
 *	@id_bustop: the unique id number of the bus.
-*	@critical_busy_bus: flag indicating whether the stopbus
-*			    already is busy with a bus. _critical region_
+*	@busy_bus: 	pointer to the stopped bus
 *	@passenger:	the number of passengers at bus
 *			(ENOPASSENGER if there is no passenger)
 *	@port_status:	flag indicating whether the door of the
@@ -36,8 +34,8 @@ typedef struct busstop	busstop_t;
 
 struct busstop {
 	uint32_t	id_bustop;
-	uint8_t		critical_busy_bus;
-	int32_t		passenger;
+	bus_t		*critical_busy_bus;
+	uint32_t	passenger;
 	uint8_t		port_status;
 	pthread_t	*exec_busstop;
 	list		*critical_ready_passengers;
@@ -70,18 +68,79 @@ busstop_t *create_busstop(pthread_t *bus_thread);
 *
 *	@stop: the pointer to the stopbus to be checked.
 */
-inline uint8_t empty_busstop(bustop_ *stop);
+inline uint8_t empty_busstop(bustop_t *stop);
 
-void	busstop_destroy(busstop_t *busstop);
+/**
+*	busstop_destroy - destroys the given busstop and then  
+*			  exits the corresponding thread.
+*			  
+*	@busstop: the busstop to be destroyed
+*/
+void 	busstop_destroy(busstop_t *busstop);
 
-passenger_t *acquire_passenger(void);
-bus_t	*acquire_bus(void);
-int	do_departure(void);
-static void 	list_ready_add(passenger_t *passenger);
-static void	list_blocked_add(passenger_t *passenger);
-static passenger_t *list_ready_first(void);
-static passenger_t *list_blocked_first(void);
-void acquire_passenger(passenger_t *pass);
-passenger_t release_passenger(void);
-busstop_t *self_bus(pthread_t *bus_thread);
+/**
+*	acquire_bus - try to return the stopped bus at a given busstop
+*	
+*	@_busstop - the busstop pointing to the stopped bus.
+*
+*	return a ppointer to the bus stopped there, or NULL otherwise.
+*/
+bus_t	*acquire_bus(busstop_t *_busstop);
+
+/**
+*	do_departure - departures the first available passenger.
+*		       i.e.: insert at stopped bus the next passenger
+*/
+void	do_departure(busstop_t *_busstop);
+
+/**
+*	list_ready_add - adds a @_passenger to the list_ready
+*
+*	this function SHALL NOT be called from other files
+*/
+static void 	list_ready_add(passenger_t *_passenger);
+
+/**
+*	list_blocked_add - adds a @_passenger to the list_blocked
+*
+*	this function SHALL NOT be called from other files
+*/
+static void	list_blocked_add(passenger_t *_passenger);
+
+/**
+*	list_ready_first - remove the first element of list_ready.
+*	
+*	return the removed element. SHALL NOT be called from other files
+*/
+static	passenger_t *list_ready_first(void);
+
+/**
+*	list_blocked_first - remove the first element of list_blocked
+*
+*	return the removed element. SHALL NOT be called from other files
+*/
+static	passenger_t *list_blocked_first(void);
+
+/**
+*	acquire_passenger - gets the arrived passenger from the bus and
+*			    inserts at the end of the queue of passengers
+*
+*/
+void	acquire_passenger(busstop_t *_busstop, passenger_t *pass);
+
+/**
+*	release_passenger - gives to the stopped bus the first available
+*			    passenger.
+*
+*	returns the first available passenger or NULL if there is
+*	no avaible passengers.
+*/
+passenger_t	release_passenger(void);
+
+/**
+*	self_bus - returns the corresponding struct of the
+*		   executing thread.
+*
+*/
+busstop_t	*self_bus(pthread_t *bus_thread);
 #endif	/*stopbus.h*/
